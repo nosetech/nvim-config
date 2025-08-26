4fcb62c67564dc401d632b0ae3682a7884ebd01f
Hiroyuki Nose <80944036+nosetech@users.noreply.github.com>
2025-08-26 10:32:02 +0900

    render-markdown pluginnの追加 (#2)

---
+++ nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,19 @@
+return {
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+		opts = {
+			completions = { lsp = { enabled = true } },
+		},
+		config = function(_, opts)
+			require("render-markdown").setup(opts)
+			-- Automatically render markdown files when opened
+			-- vim.api.nvim_create_autocmd("FileType", {
+			--   pattern = "markdown",
+			--   callback = function()
+			--     require("render-markdown").render()
+			--   end,
+			-- })
+		end,
+	},
+}
--- nvim/luasnippets/typescriptreact.lua
+++ nvim/luasnippets/typescriptreact.lua
@@ -6,32 +6,32 @@ local l = extras.lambda
 local fmt = require("luasnip.extras.fmt").fmt
 
 return {
-  s(
-    "us",
-    fmt("const [{},set{setter}] = useState<{}>({})", {
-      i(1, "value"),
-      i(2, "type"),
-      i(3, "initialValue"),
-      setter = l(l._1:sub(1, 1):upper() .. l._1:sub(2, -1), 1),
-    })
-  ),
-  s(
-    "ue",
-    fmt(
-      [[
+	s(
+		"us",
+		fmt("const [{},set{setter}] = useState<{}>({})", {
+			i(1, "value"),
+			i(2, "type"),
+			i(3, "initialValue"),
+			setter = l(l._1:sub(1, 1):upper() .. l._1:sub(2, -1), 1),
+		})
+	),
+	s(
+		"ue",
+		fmt(
+			[[
   useEffect(() => {{
     {}
   }}, [])
   ]],
-      {
-        i(1),
-      }
-    )
-  ),
-  s(
-    "comp",
-    fmt(
-      [[
+			{
+				i(1),
+			}
+		)
+	),
+	s(
+		"comp",
+		fmt(
+			[[
   'use client'
 
   export type {typeName}Props = {typeDef}
@@ -44,45 +44,64 @@ return {
     )
   }}
   ]],
-      {
-        compName = i(1, "ComponentName"),
-        typeDef = i(2, "TypeDefinition"),
-        typeName = extras.rep(1),
-        i(3),
-      }
+			{
+				compName = i(1, "ComponentName"),
+				typeDef = i(2, "TypeDefinition"),
+				typeName = extras.rep(1),
+				i(3),
+			}
+		)
+	),
+	s(
+		"page",
+		fmt(
+			[[
+  'use client'
+
+  export default function Page() {{
+    {}
+
+    return (
+      <> </>
     )
-  ),
-  s(
-    "ternary",
-    fmt("{cond} ? {node1} : {node2}", {
-      cond = i(1, "cond"),
-      node1 = i(2, "then"),
-      node2 = i(3, "else"),
-    })
-  ),
-  s(
-    "cl",
-    fmt("console.log({})", {
-      i(1),
-    })
-  ),
-  s(
-    "clv",
-    fmt("console.log('{var1}: ', {var2})", {
-      var1 = i(1, "variable"),
-      var2 = extras.rep(1),
-    })
-  ),
-  s(
-    "todo",
-    fmt("// TODO: {}", {
-      i(1, "comment"),
-    })
-  ),
-  s(
-    "note",
-    fmt("// NOTE: {}", {
-      i(1, "comment"),
-    })
-  ),
+  }}
+  ]],
+			{
+				i(1),
+			}
+		)
+	),
+	s(
+		"ternary",
+		fmt("{cond} ? {node1} : {node2}", {
+			cond = i(1, "cond"),
+			node1 = i(2, "then"),
+			node2 = i(3, "else"),
+		})
+	),
+	s(
+		"cl",
+		fmt("console.log({})", {
+			i(1),
+		})
+	),
+	s(
+		"clv",
+		fmt("console.log('{var1}: ', {var2})", {
+			var1 = i(1, "variable"),
+			var2 = extras.rep(1),
+		})
+	),
+	s(
+		"todo",
+		fmt("// TODO: {}", {
+			i(1, "comment"),
+		})
+	),
+	s(
+		"note",
+		fmt("// NOTE: {}", {
+			i(1, "comment"),
+		})
+	),
 }
